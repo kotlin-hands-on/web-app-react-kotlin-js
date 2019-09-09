54b762afb655663276c2e61bfdbf32b39a95d9df
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2019-09-09 17:40:12 +0200

    Add VideoPlayer standin component Add button to video player

--- src/App.kt
+++ src/App.kt
@@ -7,11 +7,25 @@ import styled.css
 import styled.styledDiv
 import styled.styledH1
 
-interface AppState: RState {
+interface AppState : RState {
     var currentVideo: Video?
+    var unwatchedVideos: List<Video>
+    var watchedVideos: List<Video>
 }
 
 class App: RComponent<RProps, AppState>() {
+
+    override fun AppState.init() {
+        unwatchedVideos = listOf(
+                Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+                Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+                Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+        )
+        watchedVideos = listOf(
+                Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+        )
+    }
+
     override fun RBuilder.render() {
         styledH1 {
             css {
@@ -24,7 +38,7 @@ class App: RComponent<RProps, AppState>() {
                 +"Videos to watch"
             }
             videoList {
-                videos = unwatchedVideos
+                videos = state.unwatchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video ->
                     setState {
@@ -37,7 +51,7 @@ class App: RComponent<RProps, AppState>() {
                 +"Videos watched"
             }
             videoList {
-                videos = watchedVideos
+                videos = state.watchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video ->
                     setState {
@@ -46,18 +60,23 @@ class App: RComponent<RProps, AppState>() {
                 }
             }
         }
-        styledDiv {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+        state.currentVideo?.let { currentVideo ->
+            videoPlayer {
+                video = currentVideo
+                unwatchedVideo = currentVideo in state.unwatchedVideos
+                onWatchedButtonPressed = {
+                    if(video in state.unwatchedVideos) {
+                        setState {
+                            unwatchedVideos -= video
+                            watchedVideos += video
+                        }
+                    }
+                    else {
+                        setState {
+                            watchedVideos -= video
+                            unwatchedVideos += video
+                        }
+                    }
                 }
             }
         }
