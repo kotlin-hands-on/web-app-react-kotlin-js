45a7cd272387d46c8eebff8c41d64d1ae1360cba
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2019-09-10 11:29:15 +0200

    Add coroutines to dependencies Use RESTful API for real video data Add video fetching functionality via coroutines

--- src/App.kt
+++ src/App.kt
@@ -1,11 +1,26 @@
-import kotlinx.css.*
+import kotlinx.coroutines.*
+import kotlinx.css.fontFamily
 import react.*
 import react.dom.div
 import react.dom.h3
-import react.dom.img
 import styled.css
-import styled.styledDiv
 import styled.styledH1
+import kotlin.browser.window
+
+suspend fun fetchVideo(id: Int): Video =
+        window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+                .await()
+                .json()
+                .await()
+                .unsafeCast<Video>()
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map { id ->
+        async {
+            fetchVideo(id)
+        }
+    }.awaitAll()
+}
 
 interface AppState : RState {
     var currentVideo: Video?
@@ -16,14 +31,16 @@ interface AppState : RState {
 class App: RComponent<RProps, AppState>() {
 
     override fun AppState.init() {
-        unwatchedVideos = listOf(
-                Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-                Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-                Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-        )
-        watchedVideos = listOf(
-                Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-        )
+        unwatchedVideos = listOf()
+        watchedVideos = listOf()
+
+        val mainScope = MainScope()
+        mainScope.launch {
+            val videos = fetchVideos()
+            setState {
+                unwatchedVideos = videos
+            }
+        }
     }
 
     override fun RBuilder.render() {
